b2e275d91e2666c47812395785429fe29ef768ed
DaKnOb <daknob.mac@gmail.com>
2018-02-02 16:25:44 +0200

    Add decryption command line tool

---
+++ cmd/decrypt/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+
+	"github.com/keybase/go-triplesec"
+
+	"github.com/Sirupsen/logrus"
+)
+
+func main() {
+
+	/* Output tool start log */
+	logrus.Printf("Starting the eldim decryption tool...")
+
+	/* Command line flags with all needed data */
+	inputFile := flag.String("in", "input.dat", "The encrypted file to decrypt.")
+	outputFile := flag.String("out", "output.dat", "The file to save the decrypted data.")
+	encryptionKey := flag.String("key", "Insecure", "The encryption password to decrypt the data.")
+
+	flag.Parse()
+
+	/* Print input and output file to log for documentation purposes */
+	logrus.Printf("Started with input file \"%s\" and output file \"%s\"", *inputFile, *outputFile)
+
+	/* Delete any file that exists in the outputFile */
+	os.Remove(*outputFile)
+
+	/* Read the encrypted file to RAM */
+	encData, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		logrus.Fatalf("Failed to read input file: %v", err)
+	}
+
+	/* Create an output file */
+	f, err := os.Create(*outputFile)
+	if err != nil {
+		logrus.Fatalf("Failed to create output file: %v", err)
+	}
+
+	logrus.Printf("Decrypting data...")
+
+	/* Create a new TripleSec cipher */
+	cipher, err := triplesec.NewCipher([]byte(*encryptionKey), nil)
+	if err != nil {
+		logrus.Fatalf("Failed to initialize the cryptographic engine: %v", err)
+	}
+
+	/* Decrypt the data in memory */
+	dec, err := cipher.Decrypt(encData)
+	if err != nil {
+		logrus.Fatalf("Decryption failed: %v", err)
+	}
+
+	logrus.Printf("Decryption completed.")
+	logrus.Printf("Writting to file...")
+
+	/* Write decrypted data to the output file */
+	_, err = f.Write(dec)
+	if err != nil {
+		logrus.Fatalf("Failed to write to file: %v", err)
+	}
+
+	/* Close the output file */
+	err = f.Close()
+	if err != nil {
+		logrus.Fatalf("Failed to finalize file write: %v", err)
+	}
+
+	/* Done */
+	logrus.Printf("Done.")
+
+}
